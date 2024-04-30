8a430cbf705fea43709b3893b1de0193a958793d
dianacamposrosas <78067733+dianacamposrosas@users.noreply.github.com>
2024-04-30 08:41:35 -0600

    Interfaces

--- shape/Square.java
+++ shape/Square.java
@@ -1,19 +1,22 @@
 package shape;
 import java.util.Scanner;
 
-public class Square extends Shape {
+public class Square implements ShapeInterface {
     Scanner leer = new Scanner(System.in);
+    int side;
     
+    public void obtenerLado(){
+        System.out.println("Please enter the side of the square (int)");
+        side= leer.nextInt();
+    }
     @Override
     public void calcularPerimetro(){
-        System.out.println("Please enter the base of the square (int)");
-        base= leer.nextInt();
         
-        System.out.println("The perimeter is: "+ (base*4));
+        System.out.println("The perimeter is: "+ (side*4)+"cm");
     }
     
     @Override
     public void calcularArea(){
-        System.out.println("The perimeter is: "+ (base*base));
+        System.out.println("The area is: "+ Math.pow(side, 2)+"cm2");
     }
 }
